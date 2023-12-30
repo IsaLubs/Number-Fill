74f09bbb9d7bf31cd216e1a2cdf433d59a4cfd69
IsaLubs <>
2023-12-30 17:06:42 +0000

    Add range amd grid size to return adjacent cell

--- run.py
+++ run.py
@@ -18,4 +18,12 @@ def save_game(grid, memory, pos, alread_pos):
         game_state = pickle.load(f)
     return game_state['grid'], game_state['memory'], game_state['pos'], game_state['alread_pos']
 
+def get_adjacent_cells(row, col, grid_size):
+    adjacent_cells = []
 
+    for i in range(row - 1, row + 2):
+        for j in range(col - 1, col + 2):
+            if 1 <= i <= grid_size and 1 <= j <= grid_size and (i != row or j != col):
+                adjacent_cells.append((i, j))
+
+    return adjacent_cells
